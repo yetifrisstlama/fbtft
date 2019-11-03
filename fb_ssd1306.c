bdb08e2e498a5a0ca34faf9c9d5bd5b398631d89
Michael Betz <michibetz@gmail.com>
2019-11-03 12:50:35 -0800

    initial

--- fb_ssd1306.c
+++ fb_ssd1306.c
@@ -1,21 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0+
 /*
  * FB driver for the SSD1306 OLED Controller
  *
  * Copyright (C) 2013 Noralf Tronnes
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
 #include <linux/module.h>
@@ -28,25 +15,22 @@
 
 #define DRVNAME		"fb_ssd1306"
 #define WIDTH		128
-#define HEIGHT		64
-
+#define HEIGHT		32
 
 /*
-  write_reg() caveat:
-
-     This doesn't work because D/C has to be LOW for both values:
-       write_reg(par, val1, val2);
-
-     Do it like this:
-       write_reg(par, val1);
-       write_reg(par, val2);
-*/
+ * write_reg() caveat:
+ *
+ * This doesn't work because D/C has to be LOW for both values:
+ * write_reg(par, val1, val2);
+ *
+ * Do it like this:
+ * write_reg(par, val1);
+ * write_reg(par, val2);
+ */
 
 /* Init sequence taken from the Adafruit SSD1306 Arduino library */
 static int init_display(struct fbtft_par *par)
 {
-	fbtft_par_dbg(DEBUG_INIT_DISPLAY, par, "%s()\n", __func__);
-
 	par->fbtftops.reset(par);
 
 	if (par->gamma.curves[0] == 0) {
@@ -65,12 +49,9 @@ static int init_display(struct fbtft_par *par)
 	write_reg(par, 0xD5);
 	write_reg(par, 0x80);
 
-	/* Set Multiplex Ratio */
+	/* Set Multiplex Ratio (number of rows) */
 	write_reg(par, 0xA8);
-	if (par->info->var.yres == 64)
-		write_reg(par, 0x3F);
-	else
-		write_reg(par, 0x1F);
+	write_reg(par, par->info->var.yres - 1);
 
 	/* Set Display Offset */
 	write_reg(par, 0xD3);
@@ -84,27 +65,34 @@ static int init_display(struct fbtft_par *par)
 	/* A[2] = 1b, Enable charge pump during display on */
 	write_reg(par, 0x14);
 
-	/* Set Memory Addressing Mode */
-	write_reg(par, 0x20);
-	/* Vertical addressing mode  */
-	write_reg(par, 0x01);
+	// Set Segment Re-map (mirror image vertically)
+	if (par->pdata->rotate >= 180)
+		write_reg(par, 0xA1);  // column address 127 is mapped to SEG0
+	else
+		write_reg(par, 0xA0);
 
-	/*Set Segment Re-map */
-	/* column address 127 is mapped to SEG0 */
-	write_reg(par, 0xA0 | 0x1);
-
-	/* Set COM Output Scan Direction */
-	/* remapped mode. Scan from COM[N-1] to COM0 */
-	write_reg(par, 0xC8);
+	// Set COM Output Scan Direction (mirror image horizontally)
+	if (par->pdata->rotate >= 180)
+		write_reg(par, 0xC8);	// COM remapped scan from COM[N-1] to COM0
+	else
+		write_reg(par, 0xC0);	// COM normal
 
 	/* Set COM Pins Hardware Configuration */
 	write_reg(par, 0xDA);
 	if (par->info->var.yres == 64)
 		/* A[4]=1b, Alternative COM pin configuration */
 		write_reg(par, 0x12);
+	else if (par->info->var.yres == 48)
+		/* A[4]=1b, Alternative COM pin configuration */
+		write_reg(par, 0x12);
 	else
 		/* A[4]=0b, Sequential COM pin configuration */
 		write_reg(par, 0x02);
+
+	// Set Memory Addressing Mode
+	write_reg(par, 0x20);
+	// Vertical addressing mode
+	write_reg(par, 0x01);
 
 	/* Set Pre-charge Period */
 	write_reg(par, 0xD9);
@@ -120,8 +108,9 @@ static int init_display(struct fbtft_par *par)
 	write_reg(par, 0xA4);
 
 	/* Set Normal Display
-	   0 in RAM: OFF in display panel
-	   1 in RAM: ON in display panel */
+	 * 0 in RAM: OFF in display panel
+	 * 1 in RAM: ON in display panel
+	 */
 	write_reg(par, 0xA6);
 
 	/* Set Display ON */
@@ -130,23 +119,50 @@ static int init_display(struct fbtft_par *par)
 	return 0;
 }
 
+static void set_addr_win_64x48(struct fbtft_par *par)
+{
+	// 64 columns starting at 32, 6 pages of 8 rows starting at 0
+	/* Set Column Address */
+	write_reg(par, 0x21);
+	write_reg(par, 0x20);
+	write_reg(par, 0x5F);
+
+	/* Set Page Address */
+	write_reg(par, 0x22);
+	write_reg(par, 0x0);
+	write_reg(par, 0x5);
+}
+
+static void set_addr_win_top_left(struct fbtft_par *par)
+{
+	// `xres` columns starting from 0, `yres` / 8 pages starting from 0
+	// Set Column pointer wrapping range
+	write_reg(par, 0x21);
+	write_reg(par, 0x00);  // column start
+	write_reg(par, par->info->var.xres - 1);  // column end
+
+	// Set Page pointer wrapping range
+	write_reg(par, 0x22);
+	write_reg(par, 0x0);  // page start
+	write_reg(par, par->info->var.yres / 8 - 1);  // page end
+}
+
 static void set_addr_win(struct fbtft_par *par, int xs, int ys, int xe, int ye)
 {
-	fbtft_par_dbg(DEBUG_SET_ADDR_WIN, par,
-		"%s(xs=%d, ys=%d, xe=%d, ye=%d)\n", __func__, xs, ys, xe, ye);
+	// always do full updates, so we just want to reset the write pointer
+	// the only way to do this seems to be the 0x21 and 0x22 commands
 
-	/* Set Lower Column Start Address for Page Addressing Mode */
-	write_reg(par, 0x00 | 0x0);
-	/* Set Higher Column Start Address for Page Addressing Mode */
-	write_reg(par, 0x10 | 0x0);
-	/* Set Display Start Line */
-	write_reg(par, 0x40 | 0x0);
+	// Set display addressing mode (wrapping of row / column pointers)
+	if (par->info->var.xres == 64 && par->info->var.yres == 48)
+		set_addr_win_64x48(par);
+	else
+		set_addr_win_top_left(par);
 }
 
 static int blank(struct fbtft_par *par, bool on)
 {
-	fbtft_par_dbg(DEBUG_BLANK, par, "%s(blank=%s)\n",
-		__func__, on ? "true" : "false");
+	fbtft_par_dbg(DEBUG_BLANK, par, "(%s=%s)\n",
+		      __func__, on ? "true" : "false");
 
 	if (on)
 		write_reg(par, 0xAE);
@@ -156,10 +172,8 @@ static int blank(struct fbtft_par *par, bool on)
 }
 
 /* Gamma is used to control Contrast */
-static int set_gamma(struct fbtft_par *par, unsigned long *curves)
+static int set_gamma(struct fbtft_par *par, u32 *curves)
 {
-	fbtft_par_dbg(DEBUG_INIT_DISPLAY, par, "%s()\n", __func__);
-
 	/* apply mask */
 	curves[0] &= 0xFF;
 
@@ -172,33 +186,34 @@ static int set_gamma(struct fbtft_par *par, unsigned long *curves)
 
 static int write_vmem(struct fbtft_par *par, size_t offset, size_t len)
 {
-	u16 *vmem16 = (u16 *)par->info->screen_base;
+	// Don't do partial updates, always write full buffer
+	u16 *vmem16 = (u16 *)par->info->screen_buffer;
+	u32 xres = par->info->var.xres;
+	u32 yres = par->info->var.yres;
 	u8 *buf = par->txbuf.buf;
 	int x, y, i;
 	int ret = 0;
 
-	fbtft_par_dbg(DEBUG_WRITE_VMEM, par, "%s()\n", __func__);
-
-	for (x = 0; x < par->info->var.xres; x++) {
-		for (y = 0; y < par->info->var.yres/8; y++) {
+	for (x = 0; x < xres; x++) {
+		// Then write rows (right)
+		for (y = 0; y < yres / 8; y++) {
+			// Write columns first (down)
 			*buf = 0x00;
 			for (i = 0; i < 8; i++)
-				*buf |= (vmem16[(y*8+i)*par->info->var.xres+x] ? 1 : 0) << i;
+				*buf |= (vmem16[(y * 8 + i) * xres + x] ? 1 : 0) << i;
 			buf++;
 		}
 	}
 
 	/* Write data */
 	gpio_set_value(par->gpio.dc, 1);
-	ret = par->fbtftops.write(par, par->txbuf.buf,
-				par->info->var.xres*par->info->var.yres/8);
+	ret = par->fbtftops.write(par, par->txbuf.buf, xres * yres / 8);
 	if (ret < 0)
-		dev_err(par->info->device,
-			"%s: write failed and returned: %d\n", __func__, ret);
+		dev_err(par->info->device, "write failed and returned: %d\n",
+			ret);
 
 	return ret;
 }
-
 
 static struct fbtft_display display = {
 	.regwidth = 8,
@@ -215,7 +230,6 @@ static struct fbtft_display display = {
 		.set_gamma = set_gamma,
 	},
 };
-
 
 FBTFT_REGISTER_DRIVER(DRVNAME, "solomon,ssd1306", &display);
 
--- fbtft-io.c
+++ fbtft-io.c
@@ -1,10 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0
 #include <linux/export.h>
 #include <linux/errno.h>
 #include <linux/gpio.h>
 #include <linux/spi/spi.h>
-#ifdef CONFIG_ARCH_BCM2708
-#include <mach/platform.h>
-#endif
 #include "fbtft.h"
 
 int fbtft_write_spi(struct fbtft_par *par, void *buf, size_t len)
@@ -16,7 +14,7 @@ int fbtft_write_spi(struct fbtft_par *par, void *buf, size_t len)
 	struct spi_message m;
 
 	fbtft_par_dbg_hex(DEBUG_WRITE, par, par->info->device, u8, buf, len,
-		"%s(len=%d): ", __func__, len);
+			  "%s(len=%d): ", __func__, len);
 
 	if (!par->spi) {
 		dev_err(par->info->device,
@@ -25,10 +23,6 @@ int fbtft_write_spi(struct fbtft_par *par, void *buf, size_t len)
 	}
 
 	spi_message_init(&m);
-	if (par->txbuf.dma && buf == par->txbuf.buf) {
-		t.tx_dma = par->txbuf.dma;
-		m.is_dma_mapped = 1;
-	}
 	spi_message_add_tail(&t, &m);
 	return spi_sync(par->spi, &m);
 }
@@ -53,7 +47,7 @@ int fbtft_write_spi_emulate_9(struct fbtft_par *par, void *buf, size_t len)
 	u64 val, dc, tmp;
 
 	fbtft_par_dbg_hex(DEBUG_WRITE, par, par->info->device, u8, buf, len,
-		"%s(len=%d): ", __func__, len);
+			  "%s(len=%d): ", __func__, len);
 
 	if (!par->extra) {
 		dev_err(par->info->device, "%s: error: par->extra is NULL\n",
@@ -62,8 +56,7 @@ int fbtft_write_spi_emulate_9(struct fbtft_par *par, void *buf, size_t len)
 	}
 	if ((len % 8) != 0) {
 		dev_err(par->info->device,
-			"%s: error: len=%d must be divisible by 8\n",
-			__func__, len);
+			"error: len=%zu must be divisible by 8\n", len);
 		return -EINVAL;
 	}
 
@@ -79,7 +72,7 @@ int fbtft_write_spi_emulate_9(struct fbtft_par *par, void *buf, size_t len)
 			src++;
 		}
 		tmp |= ((*src & 0x0100) ? 1 : 0);
-		*(u64 *)dst = cpu_to_be64(tmp);
+		*(__be64 *)dst = cpu_to_be64(tmp);
 		dst += 8;
 		*dst++ = (u8)(*src++ & 0x00FF);
 		added++;
@@ -109,190 +102,26 @@ int fbtft_read_spi(struct fbtft_par *par, void *buf, size_t len)
 	if (par->startbyte) {
 		if (len > 32) {
 			dev_err(par->info->device,
-				"%s: len=%d can't be larger than 32 when using 'startbyte'\n",
-				__func__, len);
+				"len=%zu can't be larger than 32 when using 'startbyte'\n",
+				len);
 			return -EINVAL;
 		}
 		txbuf[0] = par->startbyte | 0x3;
 		t.tx_buf = txbuf;
 		fbtft_par_dbg_hex(DEBUG_READ, par, par->info->device, u8,
-			txbuf, len, "%s(len=%d) txbuf => ", __func__, len);
+				  txbuf, len, "%s(len=%d) txbuf => ",
+				  __func__, len);
 	}
 
 	spi_message_init(&m);
 	spi_message_add_tail(&t, &m);
 	ret = spi_sync(par->spi, &m);
 	fbtft_par_dbg_hex(DEBUG_READ, par, par->info->device, u8, buf, len,
-		"%s(len=%d) buf <= ", __func__, len);
+			  "%s(len=%d) buf <= ", __func__, len);
 
 	return ret;
 }
 EXPORT_SYMBOL(fbtft_read_spi);
-
-
-#ifdef CONFIG_ARCH_BCM2708
-
-/*
- *  Raspberry Pi
- *  -  writing directly to the registers is 40-50% faster than
- *     optimized use of gpiolib
- */
-
-#define GPIOSET(no, ishigh)           \
-do {                                  \
-	if (ishigh)                   \
-		set |= (1 << (no));   \
-	else                          \
-		reset |= (1 << (no)); \
-} while (0)
-
-int fbtft_write_gpio8_wr(struct fbtft_par *par, void *buf, size_t len)
-{
-	unsigned int set = 0;
-	unsigned int reset = 0;
-	u8 data;
-
-	fbtft_par_dbg_hex(DEBUG_WRITE, par, par->info->device, u8, buf, len,
-		"%s(len=%d): ", __func__, len);
-
-	while (len--) {
-		data = *(u8 *) buf;
-		buf++;
-
-		/* Set data */
-		GPIOSET(par->gpio.db[0], (data&0x01));
-		GPIOSET(par->gpio.db[1], (data&0x02));
-		GPIOSET(par->gpio.db[2], (data&0x04));
-		GPIOSET(par->gpio.db[3], (data&0x08));
-		GPIOSET(par->gpio.db[4], (data&0x10));
-		GPIOSET(par->gpio.db[5], (data&0x20));
-		GPIOSET(par->gpio.db[6], (data&0x40));
-		GPIOSET(par->gpio.db[7], (data&0x80));
-		writel(set, __io_address(GPIO_BASE+0x1C));
-		writel(reset, __io_address(GPIO_BASE+0x28));
-
-		/* Pulse /WR low */
-		writel((1<<par->gpio.wr),  __io_address(GPIO_BASE+0x28));
-		writel(0,  __io_address(GPIO_BASE+0x28)); /* used as a delay */
-		writel((1<<par->gpio.wr),  __io_address(GPIO_BASE+0x1C));
-
-		set = 0;
-		reset = 0;
-	}
-
-	return 0;
-}
-EXPORT_SYMBOL(fbtft_write_gpio8_wr);
-
-int fbtft_write_gpio16_wr(struct fbtft_par *par, void *buf, size_t len)
-{
-	unsigned int set = 0;
-	unsigned int reset = 0;
-	u16 data;
-
-	fbtft_par_dbg_hex(DEBUG_WRITE, par, par->info->device, u8, buf, len,
-		"%s(len=%d): ", __func__, len);
-
-	while (len) {
-		len -= 2;
-		data = *(u16 *) buf;
-		buf += 2;
-
-		/* Start writing by pulling down /WR */
-		gpio_set_value(par->gpio.wr, 0);
-
-		/* Set data */
-		GPIOSET(par->gpio.db[0],  (data&0x0001));
-		GPIOSET(par->gpio.db[1],  (data&0x0002));
-		GPIOSET(par->gpio.db[2],  (data&0x0004));
-		GPIOSET(par->gpio.db[3],  (data&0x0008));
-		GPIOSET(par->gpio.db[4],  (data&0x0010));
-		GPIOSET(par->gpio.db[5],  (data&0x0020));
-		GPIOSET(par->gpio.db[6],  (data&0x0040));
-		GPIOSET(par->gpio.db[7],  (data&0x0080));
-
-		GPIOSET(par->gpio.db[8],  (data&0x0100));
-		GPIOSET(par->gpio.db[9],  (data&0x0200));
-		GPIOSET(par->gpio.db[10], (data&0x0400));
-		GPIOSET(par->gpio.db[11], (data&0x0800));
-		GPIOSET(par->gpio.db[12], (data&0x1000));
-		GPIOSET(par->gpio.db[13], (data&0x2000));
-		GPIOSET(par->gpio.db[14], (data&0x4000));
-		GPIOSET(par->gpio.db[15], (data&0x8000));
-
-		writel(set, __io_address(GPIO_BASE+0x1C));
-		writel(reset, __io_address(GPIO_BASE+0x28));
-
-		/* Pullup /WR */
-		gpio_set_value(par->gpio.wr, 1);
-
-		set = 0;
-		reset = 0;
-	}
-
-	return 0;
-}
-EXPORT_SYMBOL(fbtft_write_gpio16_wr);
-
-int fbtft_write_gpio16_wr_latched(struct fbtft_par *par, void *buf, size_t len)
-{
-	unsigned int set = 0;
-	unsigned int reset = 0;
-	u16 data;
-
-	fbtft_par_dbg_hex(DEBUG_WRITE, par, par->info->device, u8, buf, len,
-		"%s(len=%d): ", __func__, len);
-
-	while (len) {
-		len -= 2;
-		data = *(u16 *) buf;
-		buf += 2;
-
-		/* Start writing by pulling down /WR */
-		gpio_set_value(par->gpio.wr, 0);
-
-		/* Low byte */
-		GPIOSET(par->gpio.db[0],  (data&0x0001));
-		GPIOSET(par->gpio.db[1],  (data&0x0002));
-		GPIOSET(par->gpio.db[2],  (data&0x0004));
-		GPIOSET(par->gpio.db[3],  (data&0x0008));
-		GPIOSET(par->gpio.db[4],  (data&0x0010));
-		GPIOSET(par->gpio.db[5],  (data&0x0020));
-		GPIOSET(par->gpio.db[6],  (data&0x0040));
-		GPIOSET(par->gpio.db[7],  (data&0x0080));
-		writel(set, __io_address(GPIO_BASE+0x1C));
-		writel(reset, __io_address(GPIO_BASE+0x28));
-
-		/* Pulse 'latch' high */
-		gpio_set_value(par->gpio.latch, 1);
-		gpio_set_value(par->gpio.latch, 0);
-
-		/* High byte */
-		GPIOSET(par->gpio.db[0], (data&0x0100));
-		GPIOSET(par->gpio.db[1], (data&0x0200));
-		GPIOSET(par->gpio.db[2], (data&0x0400));
-		GPIOSET(par->gpio.db[3], (data&0x0800));
-		GPIOSET(par->gpio.db[4], (data&0x1000));
-		GPIOSET(par->gpio.db[5], (data&0x2000));
-		GPIOSET(par->gpio.db[6], (data&0x4000));
-		GPIOSET(par->gpio.db[7], (data&0x8000));
-		writel(set, __io_address(GPIO_BASE+0x1C));
-		writel(reset, __io_address(GPIO_BASE+0x28));
-
-		/* Pullup /WR */
-		gpio_set_value(par->gpio.wr, 1);
-
-		set = 0;
-		reset = 0;
-	}
-
-	return 0;
-}
-EXPORT_SYMBOL(fbtft_write_gpio16_wr_latched);
-
-#undef GPIOSET
-
-#else
 
 /*
  * Optimized use of gpiolib is twice as fast as no optimization
@@ -307,10 +136,10 @@ int fbtft_write_gpio8_wr(struct fbtft_par *par, void *buf, size_t len)
 #endif
 
 	fbtft_par_dbg_hex(DEBUG_WRITE, par, par->info->device, u8, buf, len,
-		"%s(len=%d): ", __func__, len);
+			  "%s(len=%d): ", __func__, len);
 
 	while (len--) {
-		data = *(u8 *) buf;
+		data = *(u8 *)buf;
 
 		/* Start writing by pulling down /WR */
 		gpio_set_value(par->gpio.wr, 0);
@@ -323,14 +152,14 @@ int fbtft_write_gpio8_wr(struct fbtft_par *par, void *buf, size_t len)
 			for (i = 0; i < 8; i++) {
 				if ((data & 1) != (prev_data & 1))
 					gpio_set_value(par->gpio.db[i],
-								(data & 1));
+						       data & 1);
 				data >>= 1;
 				prev_data >>= 1;
 			}
 		}
 #else
 		for (i = 0; i < 8; i++) {
-			gpio_set_value(par->gpio.db[i], (data & 1));
+			gpio_set_value(par->gpio.db[i], data & 1);
 			data >>= 1;
 		}
 #endif
@@ -339,7 +168,7 @@ int fbtft_write_gpio8_wr(struct fbtft_par *par, void *buf, size_t len)
 		gpio_set_value(par->gpio.wr, 1);
 
 #ifndef DO_NOT_OPTIMIZE_FBTFT_WRITE_GPIO
-		prev_data = *(u8 *) buf;
+		prev_data = *(u8 *)buf;
 #endif
 		buf++;
 	}
@@ -357,10 +186,10 @@ int fbtft_write_gpio16_wr(struct fbtft_par *par, void *buf, size_t len)
 #endif
 
 	fbtft_par_dbg_hex(DEBUG_WRITE, par, par->info->device, u8, buf, len,
-		"%s(len=%d): ", __func__, len);
+			  "%s(len=%d): ", __func__, len);
 
 	while (len) {
-		data = *(u16 *) buf;
+		data = *(u16 *)buf;
 
 		/* Start writing by pulling down /WR */
 		gpio_set_value(par->gpio.wr, 0);
@@ -373,14 +202,14 @@ int fbtft_write_gpio16_wr(struct fbtft_par *par, void *buf, size_t len)
 			for (i = 0; i < 16; i++) {
 				if ((data & 1) != (prev_data & 1))
 					gpio_set_value(par->gpio.db[i],
-								(data & 1));
+						       data & 1);
 				data >>= 1;
 				prev_data >>= 1;
 			}
 		}
 #else
 		for (i = 0; i < 16; i++) {
-			gpio_set_value(par->gpio.db[i], (data & 1));
+			gpio_set_value(par->gpio.db[i], data & 1);
 			data >>= 1;
 		}
 #endif
@@ -389,7 +218,7 @@ int fbtft_write_gpio16_wr(struct fbtft_par *par, void *buf, size_t len)
 		gpio_set_value(par->gpio.wr, 1);
 
 #ifndef DO_NOT_OPTIMIZE_FBTFT_WRITE_GPIO
-		prev_data = *(u16 *) buf;
+		prev_data = *(u16 *)buf;
 #endif
 		buf += 2;
 		len -= 2;
@@ -405,5 +234,3 @@ int fbtft_write_gpio16_wr_latched(struct fbtft_par *par, void *buf, size_t len)
 	return -1;
 }
 EXPORT_SYMBOL(fbtft_write_gpio16_wr_latched);
-
-#endif /* CONFIG_ARCH_BCM2708 */
